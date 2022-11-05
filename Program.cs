16c59e8b516f0018f6790351dfa4871f4a39382e
Carlos Decker <carlosdecker022@gmail.com>
2022-11-04 21:25:23 -0300

    Exercício completo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int Inicio = 0;
+Console.ForegroundColor = ConsoleColor.White;
+System.Console.Write("Números pares entre 0 e ");
+int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Red;
+while (Inicio <= numeroDigitado)
+{
+       System.Console.Write($"{Inicio} ");
+    Inicio += 2;
+}
+
+Console.ResetColor();
+System.Console.WriteLine();
